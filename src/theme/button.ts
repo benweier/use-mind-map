149705d4145c771a4328463066758c59843f3051
Ben Weier <ben.weier@gmail.com>
2022-04-29 09:56:09 +1000

    create chakra theme

---
+++ src/theme/button.ts
@@ -0,0 +1,10 @@
+import type { ComponentStyleConfig } from '@chakra-ui/theme'
+
+export const Button: ComponentStyleConfig = {
+  variants: {
+    primary: {
+      color: 'white',
+      bgColor: 'blue.500',
+    },
+  },
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react'
+import { Button } from './button'
+
+export const theme = extendTheme({
+  components: {
+    Button,
+  },
+})
