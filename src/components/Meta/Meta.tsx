4f2a0fe4f53e54d8ea31144a1bb6ee883252b568
Ben Weier <ben.weier@gmail.com>
2022-04-30 11:59:35 +1000

    update meta title template

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet-async'
 
 export const Meta = ({ title, children }: { title: string; children?: ReactNode }) => {
   return (
-    <Helmet titleTemplate="%s | useMinMap">
+    <Helmet titleTemplate="%s | useMindMap">
       <title>{title}</title>
       {children}
     </Helmet>
