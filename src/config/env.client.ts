566cb56258aa1102f7c80c2ef1612dc5fe1a03cc
Ben Weier <ben.weier@gmail.com>
2022-05-08 13:20:21 +1000

    create env config

---
+++ src/config/env.client.ts
@@ -0,0 +1,5 @@
+export const env = {
+  APPWRITE_ENDPOINT: process.env.APPWRITE_ENDPOINT ?? '',
+  APPWRITE_PROJECT_ID: process.env.APPWRITE_PROJECT_ID ?? '',
+  APPWRITE_FUNCTION_MINDMAP: process.env.APPWRITE_FUNCTION_MINDMAP ?? '',
+}
