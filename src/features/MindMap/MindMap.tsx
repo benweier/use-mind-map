51e9f4e554407d37a97dd0fa4f44e30b4f570afb
Ben Weier <ben.weier@gmail.com>
2022-05-08 21:34:13 +1000

    add edge state event handlers

--- src/features/MindMap/MindMap.tsx
+++ src/features/MindMap/MindMap.tsx
@@ -1,34 +1,57 @@
 import { memo } from 'react'
 import { Box } from '@chakra-ui/react'
+import { nanoid } from 'nanoid/non-secure'
 import ReactFlow, {
   Background,
+  Connection,
   Controls,
   Edge,
-  MiniMap,
   Node,
   ReactFlowProvider,
+  addEdge,
+  updateEdge,
   useEdgesState,
   useNodesState,
 } from 'react-flow-renderer'
 import { useMutation } from 'react-query'
 import { useParams } from 'react-router'
-import { deleteMindmapNodes, updateMindmapNode } from '@/services/api'
+import {
+  createMindmapEdge,
+  deleteMindmapEdges,
+  deleteMindmapNodes,
+  updateMindmapEdge,
+  updateMindmapNode,
+} from '@/services/api'
 import { MindMapActions } from './Actions'
 import { useMindMapSubscription } from './useMindMapSubscription'
+
+const createEdge = (source: string, target: string): Edge => ({
+  id: `edge-${nanoid(9)}`,
+  source,
+  target,
+})
 
 const Flow = ({ workspace, id }: { workspace: string; id: string }) => {
   const { data, isSuccess } = useMindMapSubscription(workspace, id)
   const onNodeDragStop = useMutation((node: Node<{ label: string }>) => updateMindmapNode(workspace, id, node))
   const onNodesDelete = useMutation((nodes: Node<{ label: string }>[]) => deleteMindmapNodes(workspace, id, nodes))
+  const onEdgeCreate = useMutation(async (edge: Edge) => createMindmapEdge(workspace, id, edge))
+  const onEdgeUpdateEnd = useMutation(async (edge: Edge) => updateMindmapEdge(workspace, id, edge))
+  const onEdgesDelete = useMutation(async (edges: Edge[]) => deleteMindmapEdges(workspace, id, edges))
 
   return (
     <>
       {isSuccess && (
         <MindMapFlow
+          workspace={workspace}
+          id={id}
           nodes={data.nodes.map((node) => JSON.parse(node) as Node)}
           edges={data.edges.map((edge) => JSON.parse(edge) as Edge)}
           onNodeDragStop={onNodeDragStop.mutate}
           onNodesDelete={onNodesDelete.mutate}
+          onEdgeCreate={onEdgeCreate.mutate}
+          onEdgeUpdateEnd={onEdgeUpdateEnd.mutate}
+          onEdgesDelete={onEdgesDelete.mutate}
         />
       )}
     </>
@@ -38,7 +61,6 @@ const Flow = ({ workspace, id }: { workspace: string; id: string }) => {
 const FlowContainer = ({ workspace, id }: { workspace: string; id: string }) => {
   return (
     <ReactFlowProvider>
-      <MindMapActions workspace={workspace} id={id} />
       <Flow workspace={workspace} id={id} />
     </ReactFlowProvider>
   )
@@ -46,18 +68,42 @@ const FlowContainer = ({ workspace, id }: { workspace: string; id: string }) => 
 
 const MindMapFlow = memo(
   ({
+    workspace,
+    id,
     nodes,
     edges,
     onNodeDragStop,
     onNodesDelete,
+    onEdgeCreate,
+    onEdgeUpdateEnd,
+    onEdgesDelete,
   }: {
+    workspace: string
+    id: string
     nodes: Node[]
     edges: Edge[]
+    onEdgeCreate: (edge: Edge) => void
+    onEdgeUpdateEnd: (edge: Edge) => void
+    onEdgesDelete: (edges: Edge[]) => void
     onNodeDragStop: (node: Node) => void
     onNodesDelete: (nodes: Node[]) => void
   }) => {
     const [n, , onNodesChange] = useNodesState(nodes)
-    const [e, , onEdgesChange] = useEdgesState(edges)
+    const [e, setEdges, onEdgesChange] = useEdgesState(edges)
+    const onEdgeUpdate = (oldEdge: Edge, newConnection: Connection) => {
+      setEdges((edge) => {
+        return updateEdge(oldEdge, newConnection, edge)
+      })
+    }
+    const onConnect = ({ source, target }: Connection) => {
+      if (source && target) {
+        setEdges((edges) => {
+          const edge = createEdge(source, target)
+          onEdgeCreate(edge)
+          return addEdge(edge, edges)
+        })
+      }
+    }
 
     return (
       <ReactFlow
@@ -69,10 +115,14 @@ const MindMapFlow = memo(
         onEdgesChange={onEdgesChange}
         onNodeDragStop={(_event, node) => onNodeDragStop(node)}
         onNodesDelete={onNodesDelete}
+        onConnect={onConnect}
+        onEdgeUpdate={onEdgeUpdate}
+        onEdgeUpdateEnd={(_event, edge) => onEdgeUpdateEnd(edge)}
+        onEdgesDelete={onEdgesDelete}
         selectNodesOnDrag={false}
       >
+        <MindMapActions workspace={workspace} id={id} />
         <Background />
-        <MiniMap />
         <Controls />
       </ReactFlow>
     )
