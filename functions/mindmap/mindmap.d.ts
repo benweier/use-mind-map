d8a5436d9a18f65d1c82ad176c26d8f5fe48a944
Ben Weier <ben.weier@gmail.com>
2022-05-08 13:53:31 +1000

    create mindmap function

---
+++ functions/mindmap/mindmap.d.ts
@@ -0,0 +1,64 @@
+import { Models } from 'node-appwrite'
+
+type NodeAttributes = 'position' | 'label' | 'size'
+type EdgeAttributes = 'source' | 'target'
+
+declare type MindMapNodeEventType = `node.create` | `node.update.${NodeAttributes}` | `node.delete`
+declare type MindMapEdgeEventType = `edge.create` | `edge.update.${EdgeAttributes}` | `edge.delete`
+
+declare interface MindMapReference {
+  collection: string
+  document: string
+}
+
+declare interface MindMapCreateNodeEvent extends MindMapReference {
+  type: 'node.create'
+  node: {
+    id: string
+    data: { label: string }
+    position: { x: number; y: number }
+    width: number
+    height: number
+  }
+}
+
+declare interface MindMapMoveNodeEvent extends MindMapReference {
+  type: 'node.update.position'
+  node: {
+    id: string
+    position: { x: number; y: number }
+  }
+}
+
+declare interface MindMapResizeNodeEvent extends MindMapReference {
+  type: 'node.update.size'
+  node: {
+    id: string
+    width: number
+    height: number
+  }
+}
+
+declare interface MindMapDeleteNodeEvent extends MindMapReference {
+  type: 'node.delete'
+  nodes: string[]
+}
+
+declare interface MindMapEdgeEventTypePayload extends MindMapReference {
+  type: MindMapEdgeEventType
+  edge: string
+}
+
+declare type MindMapEventPayload =
+  | MindMapCreateNodeEvent
+  | MindMapMoveNodeEvent
+  | MindMapResizeNodeEvent
+  | MindMapDeleteNodeEvent
+
+declare interface MindMapDocument extends Models.Document {
+  name: string
+  description: string
+  nodes: string[]
+  edges: string[]
+  tags: string[]
+}
---
+++ functions/mindmap/src/index.js
@@ -0,0 +1,84 @@
+/// <reference types="../../functions" />
+/// <reference types="../mindmap" />
+
+const sdk = require('node-appwrite')
+
+/** @type {FunctionsHandler<sdk.Models.Document | undefined>} */
+const handler = async (req, res) => {
+  if (
+    ![
+      req.env.APPWRITE_FUNCTION_ENDPOINT,
+      req.env.APPWRITE_FUNCTION_PROJECT_ID,
+      req.env.APPWRITE_FUNCTION_API_KEY,
+    ].every(Boolean)
+  ) {
+    throw new Error('Environment variables are not set')
+  }
+
+  const client = new sdk.Client()
+  const database = new sdk.Database(client)
+
+  client
+    .setEndpoint(req.env.APPWRITE_FUNCTION_ENDPOINT)
+    .setProject(req.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.env.APPWRITE_FUNCTION_API_KEY)
+    .setSelfSigned(true)
+
+  switch (req.env.APPWRITE_FUNCTION_TRIGGER) {
+    case 'http': {
+      /** @type {MindMapEventPayload} */
+      const data = JSON.parse(req.env.APPWRITE_FUNCTION_DATA)
+
+      switch (data.type) {
+        case 'node.create': {
+          /** @type {MindMapDocument} */
+          const mindmap = await database.getDocument(data.collection, data.document)
+          const nodes = mindmap.nodes.concat(JSON.stringify(data.node))
+
+          await database.updateDocument(data.collection, data.document, { nodes })
+
+          return res.send('', 204)
+        }
+
+        case 'node.update.position': {
+          /** @type {MindMapDocument} */
+          const mindmap = await database.getDocument(data.collection, data.document)
+          const nodes = mindmap.nodes.map((n) => {
+            /** @type {import('react-flow-renderer').Node} */
+            const node = JSON.parse(n)
+
+            if (node.id === data.node.id) {
+              return JSON.stringify({ ...node, position: data.node.position })
+            }
+
+            return n
+          })
+
+          await database.updateDocument(data.collection, data.document, { nodes })
+
+          return res.send('', 204)
+        }
+
+        case 'node.delete': {
+          /** @type {MindMapDocument} */
+          const mindmap = await database.getDocument(data.collection, data.document)
+          const nodes = mindmap.nodes.filter((n) => {
+            /** @type {import('react-flow-renderer').Node} */
+            const node = JSON.parse(n)
+
+            return !data.nodes.includes(node.id)
+          })
+
+          await database.updateDocument(data.collection, data.document, { nodes })
+
+          return res.send('', 204)
+        }
+      }
+    }
+
+    default:
+      throw new Error('Unsupported trigger')
+  }
+}
+
+module.exports = handler
