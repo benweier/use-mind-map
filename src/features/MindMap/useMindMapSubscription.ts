82e9bf128d8d9adf3b6c56ccd6b1c281b9b7f4ef
Ben Weier <ben.weier@gmail.com>
2022-05-09 17:56:10 +1000

    set edges from mindmap update event

--- src/features/MindMap/useMindMapSubscription.ts
+++ src/features/MindMap/useMindMapSubscription.ts
@@ -1,10 +1,10 @@
 import { useEffect, useMemo } from 'react'
-import { Node, useReactFlow } from 'react-flow-renderer'
+import { Edge, Node, useReactFlow } from 'react-flow-renderer'
 import { useQuery, useQueryClient } from 'react-query'
 import { getMindMap, subscribeToMindMap } from '@/services/api'
 
 export const useMindMapSubscription = (workspace: string, id: string) => {
-  const { setNodes } = useReactFlow()
+  const { setNodes, setEdges } = useReactFlow()
   const queryClient = useQueryClient()
   const key = useMemo(() => ['mind-map', workspace, id], [id, workspace])
   const channel = useMemo(() => `collections.${workspace}.documents.${id}`, [id, workspace])
@@ -13,11 +13,13 @@ export const useMindMapSubscription = (workspace: string, id: string) => {
     subscribeToMindMap(channel, (response) => {
       if (response.event === 'database.documents.update') {
         const nodes = response.payload.nodes.map((node) => JSON.parse(node) as Node)
+        const edges = response.payload.edges.map((edge) => JSON.parse(edge) as Edge)
         queryClient.setQueryData(key, response.payload, { updatedAt: response.timestamp })
         setNodes(nodes)
+        setEdges(edges)
       }
     })
-  }, [channel, id, key, queryClient, setNodes])
+  }, [channel, id, key, queryClient, setEdges, setNodes])
 
   return useQuery(key, () => getMindMap(workspace, id), {
     cacheTime: Infinity,
