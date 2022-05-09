6c1bd12adb021f9b2b40968f6b7d892c2c0874ef
Ben Weier <ben.weier@gmail.com>
2022-05-09 20:35:45 +1000

    remove old code

--- src/features/MindMap/MindMap.tsx
+++ src/features/MindMap/MindMap.tsx
@@ -58,14 +58,6 @@ const Flow = ({ workspace, id }: { workspace: string; id: string }) => {
   )
 }
 
-const FlowContainer = ({ workspace, id }: { workspace: string; id: string }) => {
-  return (
-    <ReactFlowProvider>
-      <Flow workspace={workspace} id={id} />
-    </ReactFlowProvider>
-  )
-}
-
 const MindMapFlow = memo(
   ({
     workspace,
@@ -131,60 +123,17 @@ const MindMapFlow = memo(
 
 MindMapFlow.displayName = 'MindMapFlow'
 
-// const NodeListener = memo(({ workspace, id }: { workspace: string; id: string }) => {
-//   const nodes = useNodes<{ label: string }>()
-//   const queryClient = useQueryClient()
-//   const data = queryClient.getQueryData<MindMapDocument>(['mind-map', workspace, id])
-//   const { mutate } = useMutation(
-//     (args: { workspace: string; id: string; data: MindMapDocument }) => {
-//       return updateMindMap(args.workspace, args.id, args.data)
-//     },
-//     {
-//       onMutate: async ({ workspace, id, data }) => {
-//         await queryClient.cancelQueries(['mind-map', workspace, id])
-//         const snapshot = queryClient.getQueryData<MindMapDocument>(['mind-map', workspace, id])
-
-//         queryClient.setQueryData(['mind-map', workspace, id], data, { updatedAt: Date.now() })
-
-//         return { snapshot }
-//       },
-//       onError: (_err, _data, context) => {
-//         queryClient.setQueryData(['mind-map', workspace, id], context?.snapshot, { updatedAt: Date.now() })
-//       },
-//       onSuccess: () => {
-//         void queryClient.invalidateQueries(['mind-map', workspace, id])
-//       },
-//     },
-//   )
-
-//   useEffect(() => {
-//     if (!data) return
-
-//     mutate({
-//       workspace,
-//       id,
-//       data: {
-//         ...data,
-//         nodes: nodes.map(({ id, data, position, height, width, type }) =>
-//           JSON.stringify({ id, data, position, height, width, type }),
-//         ),
-//         edges: [],
-//       },
-//     })
-//   }, [data, id, nodes, mutate, workspace])
-
-//   return <></>
-// })
-
-// NodeListener.displayName = 'NodeListener'
-
 export const MindMap = () => {
   const { workspace, id } = useParams()
 
   return (
     <>
       <Box maxW="100vw" maxH="100vh" w="100vw" h="100vh" overflow="hidden">
-        {workspace && id && <FlowContainer workspace={workspace} id={id} />}
+        {workspace && id && (
+          <ReactFlowProvider>
+            <Flow workspace={workspace} id={id} />
+          </ReactFlowProvider>
+        )}
       </Box>
     </>
   )
