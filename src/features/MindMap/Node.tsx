87633478f5fc2949758d790c5c900be096f28dc4
Ben Weier <ben.weier@gmail.com>
2022-05-11 22:15:34 +1000

    add custom colours to grouped handle types

--- src/features/MindMap/Node.tsx
+++ src/features/MindMap/Node.tsx
@@ -8,14 +8,19 @@ export const SimpleNode = ({ data }: NodeProps<{ label: string }>) => {
         type="target"
         position={Position.Left}
         id="target-left"
-        style={{ top: 10, bottom: 'auto', width: 10, height: 10, borderWidth: 2 }}
+        style={{ top: 10, bottom: 'auto', width: 10, height: 10, borderWidth: 2, backgroundColor: '#D53F8C' }}
       />
-      <Handle type="target" position={Position.Top} id="target-top" style={{ width: 10, height: 10, borderWidth: 2 }} />
+      <Handle
+        type="target"
+        position={Position.Top}
+        id="target-top"
+        style={{ width: 10, height: 10, borderWidth: 2, backgroundColor: '#D53F8C' }}
+      />
       <Handle
         type="target"
         position={Position.Right}
         id="target-right"
-        style={{ top: 10, bottom: 'auto', width: 10, height: 10, borderWidth: 2 }}
+        style={{ top: 10, bottom: 'auto', width: 10, height: 10, borderWidth: 2, backgroundColor: '#D53F8C' }}
       />
 
       <Box px={5} py={2} minW={200} maxW={300} border="2px" borderColor="gray.600" rounded="lg" bgColor="gray.50">
@@ -28,19 +33,19 @@ export const SimpleNode = ({ data }: NodeProps<{ label: string }>) => {
         type="source"
         position={Position.Left}
         id="source-left"
-        style={{ bottom: 0, top: 'auto', width: 10, height: 10, borderWidth: 2 }}
+        style={{ bottom: 0, top: 'auto', width: 10, height: 10, borderWidth: 2, backgroundColor: '#3182ce' }}
       />
       <Handle
         type="source"
         position={Position.Bottom}
         id="source-bottom"
-        style={{ width: 10, height: 10, borderWidth: 2 }}
+        style={{ width: 10, height: 10, borderWidth: 2, backgroundColor: '#3182ce' }}
       />
       <Handle
         type="source"
         position={Position.Right}
         id="source-right"
-        style={{ bottom: 0, top: 'auto', width: 10, height: 10, borderWidth: 2 }}
+        style={{ bottom: 0, top: 'auto', width: 10, height: 10, borderWidth: 2, backgroundColor: '#3182ce' }}
       />
     </>
   )
