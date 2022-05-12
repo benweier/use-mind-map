d998550a08628738538658999d070895fbb3a591
Ben Weier <ben.weier@gmail.com>
2022-05-12 17:50:02 +1000

    remove old on click handler

--- src/features/MindMap/Edge.tsx
+++ src/features/MindMap/Edge.tsx
@@ -1,5 +1,5 @@
 import { Flex, Icon } from '@chakra-ui/react'
-import { EdgeProps, getBezierPath, getEdgeCenter, useReactFlow } from 'react-flow-renderer'
+import { EdgeProps, getBezierPath, getEdgeCenter } from 'react-flow-renderer'
 import { HiOutlineHand } from 'react-icons/hi'
 
 export const SimpleEdge = ({
@@ -13,7 +13,6 @@ export const SimpleEdge = ({
   style = {},
   markerEnd,
 }: EdgeProps) => {
-  const { setEdges } = useReactFlow()
   const edgePath = getBezierPath({
     sourceX,
     sourceY,
@@ -37,15 +36,9 @@ export const SimpleEdge = ({
         height={20}
         x={edgeCenterX - 20 / 2}
         y={edgeCenterY - 20 / 2}
-        className="edge-button-delete"
         requiredExtensions="http://www.w3.org/1999/xhtml"
       >
-        <Flex
-          justify="center"
-          alignItems="center"
-          onClick={() => setEdges((edges) => edges.filter((edge) => edge.id === id))}
-          bgColor="white"
-        >
+        <Flex justify="center" alignItems="center" bgColor="white">
           <Icon as={HiOutlineHand} color="gray.400" fontSize={20} p={1} />
         </Flex>
       </foreignObject>
