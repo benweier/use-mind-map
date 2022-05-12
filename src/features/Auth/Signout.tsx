a9867250c61adcf3d59e2f5042dc06dc78a1b8ee
Ben Weier <ben.weier@gmail.com>
2022-05-12 17:31:36 +1000

    invalidate all queries on sign out

--- src/features/Auth/Signout.tsx
+++ src/features/Auth/Signout.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react'
-import { useMutation } from 'react-query'
+import { useMutation, useQueryClient } from 'react-query'
 import { useNavigate } from 'react-router-dom'
 import { deleteCurrentSession } from '@/services/api'
 
 export const Signout = () => {
+  const queryClient = useQueryClient()
   const navigate = useNavigate()
   const { mutate } = useMutation(deleteCurrentSession, {
     onSuccess: () => {
+      queryClient.invalidateQueries()
       navigate('/', { replace: true })
     },
   })
